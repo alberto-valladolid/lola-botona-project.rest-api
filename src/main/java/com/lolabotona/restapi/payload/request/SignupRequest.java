cc428d1400829c962cc93af40b95e3d2c66520f4
Alberto <alberiba_94@hotmail.com>
2020-03-19 00:53:01 +0100

    payloads añadidos falta authController

---
+++ src/main/java/com/lolabotona/restapi/payload/request/SignupRequest.java
@@ -0,0 +1,45 @@
+package com.lolabotona.restapi.payload.request;
+
+import javax.validation.constraints.*;
+ 
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+ 
+    @NotBlank
+    @Size(max = 50)
+    private String role;
+    
+
+    
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+  
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+
+}
