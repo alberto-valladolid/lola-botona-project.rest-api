fbc8759ecec136af4d8dae62afd22094b4051dd5
Alberto <alberiba_94@hotmail.com>
2020-04-15 19:09:59 +0200

    se ha eliminado propiedades de la clase  que no se utilizaban

--- src/main/java/com/lolabotona/restapi/model/CalendarEvent.java
+++ src/main/java/com/lolabotona/restapi/model/CalendarEvent.java
@@ -10,18 +10,22 @@ import lombok.Setter;
 public class CalendarEvent {
 
 	private boolean groupExist; 
+	private Long groupId; 
 	private boolean userAssits; 
 	private boolean isFull; 
     private Timestamp timeOfDay; // 12:00  morning - 18:00 afternoon
     private String description; 
     
     
-    public CalendarEvent(boolean groupExist,boolean userAssits, boolean isFull, Timestamp timeOfDay,String description) {
+    
+    public CalendarEvent(boolean groupExist,boolean userAssits, boolean isFull, Timestamp timeOfDay,String description, Long groupId) {
     	this.groupExist = groupExist; 
+    	this.groupId = groupId; 
     	this.userAssits = userAssits; 
     	this.isFull = isFull; 
     	this.timeOfDay = timeOfDay; 
-    	this.description = description; 
+    	this.description = description;
+		
     }
     
     public CalendarEvent() {
